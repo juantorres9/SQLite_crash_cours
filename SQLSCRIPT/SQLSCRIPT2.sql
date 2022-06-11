9a920aff81f762b9a0606390494dd10230664e18
jctor <juan.torres.zetino@gmail.com>
2022-06-11 15:52:42 +0200

    adding first sample database script syntax

---
+++ SQLSCRIPT/SQLSCRIPT2.sql
@@ -0,0 +1,138 @@
+-- display current session databses
+.databases
+-- attach databse on current session
+ATTACH DATABASE "c:\sqlite\db\chinook.db" AS chinook;
+ATTACH DATABASE "c:\sqlite\db\TEST_NAMES" AS TEST_NAMES;
+ATTACH DATABASE "c:\sqlite\db\TASK_APK_DB" AS TASK_APK_DB;
+-- show all tables of an specific database
+.tables
+-- refer to something in an attached DB NOT Main DB
+--show schema of a table
+.schema 
+-- visualiser la structuire d'une table  en specifiant le nom
+.schema albums
+.schema chinook.albums
+
+-- #NULL VALUE
+INSERT INTO contacts (contact_id,first_name,last_name,email,phone) VALUES( 99,"carlos" ,"torres","toto@gmail.fr","");
+--#NOT NULL VALUE
+INSERT INTO contacts (contact_id,first_name,last_name,email,phone) VALUES( 99,"carlos" ,"torres","toto@gmail.fr","12345");
+INSERT INTO contacts (contact_id,first_name,last_name,email,phone) VALUES( 100,"TOTO" ,"TITI","tot2o@gmail.fr","123456789");
+
+INSERT INTO tb_task (title,priority,description) VALUES( "FAIRE MUR REPARATION",1 ,"preparer mortier \n appliquer mortier");
+INSERT INTO tb_task (title,priority,description) VALUES( "INSTALLER LAMP",1 ,"");
+INSERT INTO tb_task (title,priority,description) VALUES( "Faire une promenade",1 ,"aller faire un tour a montferrand tous");
+
+--INSERT tb_task2
+INSERT INTO tb_task3 (title,priority,description) VALUES( "Faire une promenade",1 ,"aller faire un tour a montferrand tous");
+INSERT INTO tb_task3 (title,priority,description) VALUES( "Faire une promenade",1 ,"aller faire un tour a montferrand tous");
+--"INSERT INTO tb_task3 (title,priority,description) VALUES( ?,?,?)";
+
+INSERT INTO tb_task2 (title,priority,description) VALUES( "Faire une promenade",1 ,"aller faire un tour a montferrand tous");
+INSERT INTO tb_task2 (title,priority,description) VALUES( "aller chez claude et pierre",2 ,"aller faire un tour a arc et senans");
+INSERT INTO tb_task2 (title,priority,description) VALUES( "aller faire vaccin",1 ,"aller faire un vaccin au labo en couple");
+-- UPDATE STATEMENT 
+UPDATE tb_task2 SET title="Demande congé",priority=1,description="faire une demande de congé a cibest "WHERE _id=3;
+-- UPDATE MULTIPLE ROWS 
+UPDATE tb_task2 SET title="Demande congé2",priority=1,description="faire une demande de congé a cibest " WHERE priority=1;
+
+
+--INSERT single value with primary key in autoincrement table 
+INSERT INTO chinook.supplier_groups (group_name)
+VALUES
+   ('Domestic'),
+   ('Global'),
+   ('One-Time');
+
+INSERT INTO chinook.suppliers (supplier_name, group_id)
+VALUES ('HP', 2);
+
+INSERT INTO chinook.suppliers (supplier_name, group_id)
+VALUES('SOLAR-E.', 3);
+-- CONSTRAINT
+INSERT INTO chinook.suppliers (supplier_name, group_id)
+VALUES('ABC Inc.', 4);
+
+INSERT INTO chinook.suppliers (supplier_name, group_id)
+VALUES('ABC Inc.', 40);
+
+--#CREATION DU TABLE
+CREATE TABLE contacts (
+	contact_id INTEGER PRIMARY KEY,
+	first_name TEXT NOT NULL,
+	last_name TEXT NOT NULL,
+	email TEXT NOT NULL UNIQUE,
+	phone TEXT NOT NULL UNIQUE
+);
+
+--#CREATION DU TABLE
+CREATE TABLE tb_task (
+	title TEXT NOT NULL,
+	priority INTEGER NOT NULL,
+	description TEXT
+);
+--#CREATION DU TABLE
+CREATE TABLE IF NOT EXISTS tb_task2 (
+              _id INTEGER PRIMARY KEY ,
+	title TEXT NOT NULL,
+	priority INTEGER NOT NULL,
+	description TEXT
+);
+
+
+-- # CREATE WITH FOREIGN KEYS
+PRAGMA foreign_keys=ON;
+
+CREATE TABLE chinook.suppliers (
+    supplier_id   INTEGER PRIMARY KEY,
+    supplier_name TEXT    NOT NULL,
+    group_id      INTEGER NOT NULL,
+    FOREIGN KEY (group_id)
+       REFERENCES supplier_groups (group_id) 
+);
+
+CREATE TABLE chinook.supplier_groups (
+	group_id integer PRIMARY KEY,
+	group_name text NOT NULL
+);
+
+-- DROP UNE TABLE
+DROP TABLE chinook.suppliers;
+DROP TABLE chinook.supplier_groups;
+DROP TABLE TASK_APP_DB.tb_task2;
+-- AFFICHAGE DE STRUCTURE 
+.schema
+
+-- RECHERCHE SELECT  
+SELECT * FROM contacts;
+SELECT * FROM chinook.suppliers;
+SELECT * FROM chinook.supplier_groups;
+-- FAKE ROWID  incrementation by DEFAULT
+SELECT rowid FROM contacts;
+--IT WILL DISPLAY 99 ET 100 AS contact_id COLUMN IT WILL DO THE MAPPING entre rowid et contact_id column 
+
+SELECT * FROM tb_task;
+-- FAKE ROWID  incrementation by DEFAULT
+SELECT rowid FROM tb_task;
+SELECT * FROM tb_task;
+-- without rowid
+SELECT title,priority,description FROM tb_task;
+-- with rowid
+SELECT rowid,title,priority,description FROM tb_task;
+SELECT rowid,title,priority,description FROM tb_task2;
+
+SELECT title,priority,description FROM tb_task2 WHERE _id=3 LIMIT 1;
+
+-- rowid implicit alias
+https://www.sqlitetutorial.net/sqlite-primary-key/
+
+-- activation de clès extrange -> foreign keys
+PRAGMA foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+-- disable  enable
+PRAGMA foreign_keys;
+
+--DELETE STATEMENT
+DELETE * FROM chinooK.supplier_groups WHERE supplier_name="SOLAR_E";
+
+DELETE FROM tb_task2 WHERE _id=2;
---
+++ SQLSCRIPT/SQLSCRIPT3_BUDGETMETER.sql
@@ -0,0 +1,88 @@
+-- Creation ou ouverture d'une DB depuis la consoleT
+$sqlite3 testDB.db
+$sqlite3 ./DB/BUDGET_APP_DB
+-- display current session databses
+.databases
+-- attach databse on current session et creer une symlink
+ATTACH DATABASE "c:\sqlite\db\chinook.db" AS chinook;
+ATTACH DATABASE "C:\Users\jctor\EXTERNAL-PROGRAMS\sqlite-tools-win32-x86-3380500\sqlite-tools-win32-x86-3380500\BUDGET_APP_DB" AS BUDGET;
+
+-- show all tables of an specific database
+.tables
+-- refer to something in an attached DB NOT Main DB
+--show schema of a table
+.schema 
+-- visualiser la structuire d'une table  en specifiant le nom
+.schema tb_budget_history
+.schema chinook.albums
+.schema tb_budget_history
+
+
+
+CREATE TABLE IF NOT EXISTS tb_budget_history (
+    _id INTEGER PRIMARY KEY ,
+	name TEXT NOT NULL,
+	comment TEXT,
+	unixtime INTEGER NOT NULL,
+	amount REAL NOT NULL
+);
+
+-- NEW VERSION
+CREATE TABLE IF NOT EXISTS tb_budget_history (
+    _id INTEGER PRIMARY KEY ,
+	name TEXT NOT NULL,
+             year_month TEXT NOT NULL,
+	comment TEXT,
+	unixtime INTEGER NOT NULL,
+             humain_tc0_isotime TEXT NOT NULL,
+	amount REAL NOT NULL
+);
+
+-- TABLE MENSUEL: YEAR_MONTH  2022-12 , 2002-05
+CREATE TABLE IF NOT EXISTS tb_monthly_tables (
+    _id INTEGER PRIMARY KEY ,
+	year_month TEXT UNIQUE NOT NULL,
+	comment TEXT
+);
+
+INSERT INTO tb_monthly_tables (year_month,comment) VALUES( "2020-12","expenses done for groceries ");
+INSERT INTO tb_monthly_tables (year_month,comment) VALUES( "2020-05","GOOD WORKER ");
+INSERT INTO tb_monthly_tables (year_month,comment) VALUES( "2020-04","HARD WORKER MOTH  ");
+
+INSERT INTO tb_budget_history (name,comment,unixtime,amount) VALUES( "supermarket payment","expenses done for groceries " ,1644166613607, 0.0);
+INSERT INTO tb_budget_history (name,comment,unixtime,amount) VALUES( "supermarket payment","expenses done for groceries " ,1612926265000, 2.0);
+INSERT INTO tb_budget_history (name,comment,unixtime,amount) VALUES( "supermarket payment3","" ,1612926265000, 4.0);
+INSERT INTO tb_budget_history (name,unixtime,amount) VALUES( "supermarket payment4" ,1612926265000, 2.0);
+INSERT INTO tb_budget_history (name,year_month,comment,unixtime,humain_tc0_isotime,amount) VALUES( "supermarket payment","2020-05","expenses done for groceries " ,1612926265000,"2020-05-22T06:00:00Z", 2.0);
+INSERT INTO tb_budget_history (name,year_month,comment,unixtime,humain_tc0_isotime,amount) VALUES( "university expense","2020-04","expensesfor UNIVERSDITY " ,1612926265020,"2020-04-22T06:00:00Z", 3.0);
+INSERT INTO tb_budget_history (name,year_month,comment,unixtime,humain_tc0_isotime,amount) VALUES( "car expense","2020-04","expensesfor car " ,1612926265070,"2020-04-22T07:00:00Z", 5.0);
+INSERT INTO tb_budget_history (name,year_month,comment,unixtime,humain_tc0_isotime,amount) VALUES( "car expense3","2020-04","expensesfor car and plane " ,1612926265092,"2020-04-23T07:00:00Z", 5.0);
+SELECT rowid,name,comment,unixtime,amount FROM tb_budget_history;
+
+
+SELECT name,comment,unixtime,amount FROM tb_budget_history WHERE _id=1 LIMIT 1;
+SELECT * FROM tb_budget_history;
+SELECT * FROM tb_monthly_tables ;
+
+--INNER JOIN AND WHERE
+SELECT tb_monthly_tables.comment,tb_budget_history.name,tb_budget_history.amount,tb_budget_history.year_month
+ FROM tb_monthly_tables 
+     INNER JOIN tb_budget_history ON tb_monthly_tables.year_month = tb_budget_history.year_month
+ WHERE tb_monthly_tables.year_month = '2020-05';
+ 
+SELECT tb_monthly_tables.comment,tb_budget_history.name,tb_budget_history.amount,tb_budget_history.year_month
+ FROM tb_monthly_tables 
+     INNER JOIN tb_budget_history ON tb_monthly_tables.year_month = tb_budget_history.year_month
+ WHERE tb_monthly_tables.year_month = "2020-04";
+
+SELECT tb_monthly_tables.comment,tb_budget_history.name,tb_budget_history.amount,tb_budget_history.year_month FROM tb_monthly_tables INNER JOIN tb_budget_history WHERE tb_monthly_tables.year_month = tb_budget_history.year_month;
+--OUTER JOIN
+SELECT tb_monthly_tables.comment,tb_budget_history.name,tb_budget_history.amount,tb_budget_history.year_month FROM tb_monthly_tables LEFT OUTER JOIN tb_budget_history ON tb_monthly_tables.year_month ="2020-05";
+
+
+-- DROP UNE TABLE
+DROP TABLE tb_budget_history;
+
+-- DELETE ROW
+
+DELETE FROM tb_budget_history WHERE _id=2;
